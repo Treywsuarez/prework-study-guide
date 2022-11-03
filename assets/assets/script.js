3fda2b9192e53c7988fcc5edc6c5a8e97bb6eb87
Trey Suarez <treyxsuarez@gmail.com>
2022-11-03 08:32:01 +0000

    added Javascript to project

---
+++ assets/assets/script.js
@@ -0,0 +1,16 @@
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var x = 0; x< topic.length; x++) {
+    console.log(topic[x])
+}
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study Javascript!");
+} else {
+    console.log('Please try again!');
+}
+*/
